74fd88eeb3fc46a793330380b10fcc86cc9c7da4
Collin Bowdoin <70925258+IntricEight@users.noreply.github.com>
2026-05-28 23:20:04 -0700

    Added an item name limit to cure books so they aren't naming themselves Written Book

--- src/main/java/frostvein/sampires/remakepire/managers/CureBookManager.java
+++ src/main/java/frostvein/sampires/remakepire/managers/CureBookManager.java
@@ -75,7 +75,7 @@ public class CureBookManager {
     public String getCureBookName(int bookNumber, boolean itemName) {
         if (itemName && CUSTOM_BOOKS) {
             // The item name of the custom cure books
-            return this.textConfig.getString("cure-book-" + bookNumber + ".item-title", "ERROR could not find item title");
+            return this.textConfig.getString("cure-book-" + bookNumber + ".item-title", "ERROR could not find item title").substring(0, 32);
 
         } else if (CUSTOM_BOOKS) {
             // The book name of the custom cure book
