6184db3cbdb93a136379a3616617526c9601d116
Collin Bowdoin <70925258+IntricEight@users.noreply.github.com>
2026-06-19 19:13:05 -0700

    Updated Clover's version with a new texture pack

--- src/main/java/frostvein/sampires/remakepire/managers/VampireTexturePackManager.java
+++ src/main/java/frostvein/sampires/remakepire/managers/VampireTexturePackManager.java
@@ -16,8 +16,8 @@ public class VampireTexturePackManager {
     private static final String VAMPIRE_TEXTURE_PACK_SHA1_STRING = "e139890dd34f56724efcd5becb476999651ca43c";
     private static final String VAMPIRE_TEXTURE_PACK_PROMPT = "§5VampireSMP Vampire Pack\n§7This pack enhances your vampire experience!";
     // Control the human texture pack access details.
-    private static final String HUMAN_TEXTURE_PACK_URL = "https://download.mc-packs.net/pack/e0324a4c07208eb4fe9b0305f531be1c9dd90cc6.zip";
-    private static final String HUMAN_TEXTURE_PACK_SHA1_STRING = "e0324a4c07208eb4fe9b0305f531be1c9dd90cc6";
+    private static final String HUMAN_TEXTURE_PACK_URL = "https://download.mc-packs.net/pack/e9f489666b0432231b0e9efd50de3bc595f40c61.zip";
+    private static final String HUMAN_TEXTURE_PACK_SHA1_STRING = "e9f489666b0432231b0e9efd50de3bc595f40c61";
     private static final String HUMAN_TEXTURE_PACK_PROMPT = "§aBlood of the Wild Human Pack\n§7This pack enhances your human experience!";
     private final Set<UUID> playersWithVampireTexturePack = new HashSet<>(), playersWithHumanTexturePack = new HashSet<>();
 
