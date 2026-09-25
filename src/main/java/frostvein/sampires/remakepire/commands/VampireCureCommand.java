cadfdb346a9bcdfd1ef51d6f442a888bc1702747
Collin Bowdoin <70925258+IntricEight@users.noreply.github.com>
2026-09-25 10:40:09 -0700

    Added a check on preventing a single beacon being used for multiple cures to only work when cures would destroy the beacon.

--- src/main/java/frostvein/sampires/remakepire/commands/VampireCureCommand.java
+++ src/main/java/frostvein/sampires/remakepire/commands/VampireCureCommand.java
@@ -82,7 +82,7 @@ public class VampireCureCommand implements CommandExecutor {
             player.sendMessage(Component.text("You must be close to a holy beacon to perform this ritual.", NamedTextColor.RED));
             return true;
 
-        } else if (this.plugin.getForcedCureChoiceManager().isBeaconBeingUsed(nearestHolyBeacon)) {
+        } else if (this.plugin.getConfigManager().doCuresHaveLastingEffects() && this.plugin.getForcedCureChoiceManager().isBeaconBeingUsed(nearestHolyBeacon)) {
             player.sendMessage(Component.text("This holy beacon is actively being channeled toward another cursed creature.", NamedTextColor.RED));
             return true;
         }
@@ -146,7 +146,7 @@ public class VampireCureCommand implements CommandExecutor {
         player.getActivePotionEffects().forEach((effect) -> player.removePotionEffect(effect.getType()));
 
         // Check if players should be able to leave and are prevented from getting turned again
-        if (plugin.getConfigManager().doCuresHaveLastingEffects()) {
+        if (this.plugin.getConfigManager().doCuresHaveLastingEffects()) {
             player.addScoreboardTag(VampireManager.CURED_VAMPIRE_TAG);
         }
 
